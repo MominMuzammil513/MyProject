cdbb08fd707f5d1f1e24af38538a3c1f08feecc1
MominMuzammil513 <muzammilaumed@2376@gmail.com>
2023-02-09 19:51:33 +0530

    Delete old repo and add new repo

--- template-app/index.js
+++
@@ -1,5 +0,0 @@
-console.log("hi.....................");
-
-const result = document.querySelector(".Details");
-const search = document.querySelector("Search");
-const url = "thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
